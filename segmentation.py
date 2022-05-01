b66637a462ea3d4af2423949bd8f370fffe8937a
graham-arnold-uw <graham.arnold.uw@gmail.com>
2022-04-30 17:24:30 -0700

    testing credential saving

--- segmentation.py
+++ segmentation.py
@@ -4,11 +4,7 @@ from matplotlib import pyplot as plt
 import os
 
 #test change to learn git actions"
-<<<<<<< HEAD
 #conflicting edit (testing branch)_master
-=======
-#conflicting edit (testing branch)_test
->>>>>>> test
 
 #divergent edit -> no conflict
 #no conflict_test
